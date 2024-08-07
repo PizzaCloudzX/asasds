c47b0395c989bb0cfaf6fc21895df6bf57da918b
PizzaCloudzX <pizzaclouds12@gmail.com>
2024-08-07 16:55:25 -0300

    Add files via upload

---
+++ dever2.py
@@ -0,0 +1,29 @@
+mes = input("digite um mes")
+if mes ==("1"):
+    print ("janeiro")
+if mes ==("2"):
+    print ("fevereiro")
+if mes ==("3"):
+    print ("março")
+if mes ==("4"):
+    print ("abril")
+if mes ==("5"):
+    print ("maio")
+if mes ==("6"):
+    print ("junho")
+if mes ==("7"):
+    print ("julho")
+if mes ==("8"):
+    print ("agosto")
+if mes ==("9"):
+    print ("setembro")
+if mes ==("10"):
+    print ("outubro")
+if mes ==("11"):
+    print ("novembro")
+if mes ==("12"):
+    print ("dezembro")
+else:
+    ("esse mes nao existe")
+  
+
